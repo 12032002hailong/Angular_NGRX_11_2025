37a9fa854cbc7a22cf5ef8624e7a989d16564044
Hai Long <hailongkc1203@gmail.com>
2025-11-18 10:46:43 +0700

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Courses } from './courses/courses';
+import { Counter } from './counter/counter';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'courses',
+    component: Courses,
+  },
+  {
+    path: 'counter',
+    component: Counter,
+  },
+];
