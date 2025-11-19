d3a296133202b9c2ceeac3e569083575fa59a86e
Hai Long <hailongkc1203@gmail.com>
2025-11-19 16:32:29 +0700

    setup NgRx store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideState, provideStore } from '@ngrx/store';
+import { counterReducer } from './app/counter/states/counter.reducer';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [...appConfig.providers, provideStore(), provideState('counter', counterReducer)],
+});
