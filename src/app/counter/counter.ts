b364f8929dc387b243e1eb2580252996e15d7660
Hai Long <hailongkc1203@gmail.com>
2025-11-19 13:06:48 +0700

    create counter-value, counter-button

--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CounterValue } from './counter-value/counter-value';
+import { CounterButton } from './counter-button/counter-button';
 
 @Component({
   selector: 'app-counter',
-  imports: [],
+  imports: [CounterValue, CounterButton],
   templateUrl: './counter.html',
   styleUrl: './counter.scss',
 })
-export class Counter {
-
-}
+export class Counter {}
